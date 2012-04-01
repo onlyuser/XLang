94e40366f8ed89210bdf8888caa957f77dbf35b7
Jerry Chen <jerry@localhost.(none)>
2012-04-01 14:56:51 +0800

    restore indentation to look more like original

--- lib-common/src/XLangMVCView.cpp
+++ lib-common/src/XLangMVCView.cpp
@@ -39,7 +39,7 @@ namespace mvc {
 
 void MVCView::print_lisp(const node::NodeIdentIFace* _node)
 {
-    if(NULL == _node)
+    if (NULL == _node)
         return;
     switch(_node->type())
     {
@@ -62,7 +62,7 @@ void MVCView::print_lisp(const node::NodeIdentIFace* _node)
             {
                 std::cout << '(' << dynamic_cast<const node::InnerNodeIFace*>(_node)->name() << ' ';
                 size_t i;
-                for(i = 0; i < dynamic_cast<const node::InnerNodeIFace*>(_node)->child_count()-1; i++)
+                for (i = 0; i < dynamic_cast<const node::InnerNodeIFace*>(_node)->child_count()-1; i++)
                 {
                     print_lisp(dynamic_cast<const node::InnerNodeIFace*>(_node)->child(i));
                     std::cout << ' ';
@@ -106,37 +106,37 @@ int ex (nodeType *p) {
     return 0;
 }
 
-/*c----cm---ce----> drawing of leaf-nodes
+/*c----cm---ce---->                       drawing of leaf-nodes
  l leaf-info
  */
 
 /*c---------------cm--------------ce----> drawing of non-leaf-nodes
- l node-info
- * |
- * ------------- ...----
- * | | |
- * v v v
- * child1 child2 ... child-n
- * che che che
- *cs cs cs cs
+ l            node-info
+ *                |
+ *    -------------     ...----
+ *    |       |               |
+ *    v       v               v
+ * child1  child2  ...     child-n
+ *        che     che             che
+ *cs      cs      cs              cs
  *
  */
 
 void exNode
-    ( nodeType *p,
-        int c, int l, /* start column and line of node */
-        int *ce, int *cm /* resulting end column and mid of node */
+    (   nodeType *p,
+        int c, int l,        /* start column and line of node */
+        int *ce, int *cm     /* resulting end column and mid of node */
     )
 {
-    int w, h; /* node width and height */
-    char *s; /* node text */
-    int cbar; /* "real" start column of node (centred above subnodes) */
-    uint32_t k; /* child number */
-    int che, chm; /* end column and mid of children */
-    int cs; /* start column of children */
-    char word[20]; /* extended node text */
+    int w, h;           /* node width and height */
+    char *s;            /* node text */
+    int cbar;           /* "real" start column of node (centred above subnodes) */
+    uint32_t k;              /* child number */
+    int che, chm;       /* end column and mid of children */
+    int cs;             /* start column of children */
+    char word[20];      /* extended node text */
 
-    if(!p) return;
+    if (!p) return;
 
     strcpy (word, "???"); /* should never appear */
     s = word;
@@ -166,7 +166,7 @@ void exNode
     *cm = c + w / 2;
 
     /* node is leaf */
-    if(p->type() != typeOpr ||
+    if (p->type() != typeOpr ||
             dynamic_cast<const node::InnerNodeIFace*>(p)->child_count() == 0) {
         graphDrawBox (s, cbar, l);
         return;
@@ -174,13 +174,13 @@ void exNode
 
     /* node has children */
     cs = c;
-    for(k = 0; k < dynamic_cast<const node::InnerNodeIFace*>(p)->child_count(); k++) {
+    for (k = 0; k < dynamic_cast<const node::InnerNodeIFace*>(p)->child_count(); k++) {
         exNode (dynamic_cast<const node::InnerNodeIFace*>(p)->child(k), cs, l+h+eps, &che, &chm);
         cs = che;
     }
 
     /* total node width */
-    if(w < che - c) {
+    if (w < che - c) {
         cbar += (che - c - w) / 2;
         *ce = che;
         *cm = (c + che) / 2;
@@ -191,7 +191,7 @@ void exNode
 
     /* draw arrows (not optimal: children are drawn a second time) */
     cs = c;
-    for(k = 0; k < dynamic_cast<const node::InnerNodeIFace*>(p)->child_count(); k++) {
+    for (k = 0; k < dynamic_cast<const node::InnerNodeIFace*>(p)->child_count(); k++) {
         exNode (dynamic_cast<const node::InnerNodeIFace*>(p)->child(k), cs, l+h+eps, &che, &chm);
         graphDrawArrow (*cm, l+h, chm, l+h+eps-1);
         cs = che;
@@ -207,22 +207,22 @@ char graph[lmax][cmax]; /* array for ASCII-Graphic */
 int graphNumber = 0;
 
 void graphTest (int l, int c)
-{ int ok;
+{   int ok;
     ok = 1;
-    if(l < 0) ok = 0;
-    if(l >= lmax) ok = 0;
-    if(c < 0) ok = 0;
-    if(c >= cmax) ok = 0;
-    if(ok) return;
-    printf ("\n+++error: l=%d, c=%d not in drawing rectangle 0, 0 ... %d, %d",
+    if (l < 0) ok = 0;
+    if (l >= lmax) ok = 0;
+    if (c < 0) ok = 0;
+    if (c >= cmax) ok = 0;
+    if (ok) return;
+    printf ("\n+++error: l=%d, c=%d not in drawing rectangle 0, 0 ... %d, %d", 
         l, c, lmax, cmax);
     //exit (1);
 }
 
 void graphInit (void) {
     int i, j;
-    for(i = 0; i < lmax; i++) {
-        for(j = 0; j < cmax; j++) {
+    for (i = 0; i < lmax; i++) {
+        for (j = 0; j < cmax; j++) {
             graph[i][j] = ' ';
         }
     }
@@ -230,15 +230,15 @@ void graphInit (void) {
 
 void graphFinish() {
     int i, j;
-    for(i = 0; i < lmax; i++) {
-        for(j = cmax-1; j > 0 && graph[i][j] == ' '; j--);
+    for (i = 0; i < lmax; i++) {
+        for (j = cmax-1; j > 0 && graph[i][j] == ' '; j--);
         graph[i][cmax-1] = 0;
-        if(j < cmax-1) graph[i][j+1] = 0;
-        if(graph[i][j] == ' ') graph[i][j] = 0;
+        if (j < cmax-1) graph[i][j+1] = 0;
+        if (graph[i][j] == ' ') graph[i][j] = 0;
     }
-    for(i = lmax-1; i > 0 && graph[i][0] == 0; i--);
+    for (i = lmax-1; i > 0 && graph[i][0] == 0; i--);
     printf ("\n\nGraph %d:\n", graphNumber++);
-    for(j = 0; j <= i; j++) printf ("\n%s", graph[j]);
+    for (j = 0; j <= i; j++) printf ("\n%s", graph[j]);
     printf("\n");
 }
 
@@ -248,9 +248,9 @@ void graphBox (char *s, int *w, int *h) {
 }
 
 void graphDrawBox (char *s, int c, int l) {
-    uint32_t i;
+    int i;
     graphTest (l, c+strlen(s)-1+del);
-    for(i = 0; i < strlen (s); i++) {
+    for (i = 0; i < strlen (s); i++) {
         graph[l][c+i+del] = s[i];
     }
 }
@@ -260,9 +260,9 @@ void graphDrawArrow (int c1, int l1, int c2, int l2) {
     graphTest (l1, c1);
     graphTest (l2, c2);
     m = (l1 + l2) / 2;
-    while(l1 != m) { graph[l1][c1] = '|'; if(l1 < l2) l1++; else l1--; }
-    while(c1 != c2) { graph[l1][c1] = '-'; if(c1 < c2) c1++; else c1--; }
-    while(l1 != l2) { graph[l1][c1] = '|'; if(l1 < l2) l1++; else l1--; }
+    while (l1 != m) { graph[l1][c1] = '|'; if (l1 < l2) l1++; else l1--; }
+    while (c1 != c2) { graph[l1][c1] = '-'; if (c1 < c2) c1++; else c1--; }
+    while (l1 != l2) { graph[l1][c1] = '|'; if (l1 < l2) l1++; else l1--; }
     graph[l1][c1] = '|';
 }
 
