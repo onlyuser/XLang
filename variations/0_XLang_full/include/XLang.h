a28e1b38de08aefdc87496a5ceb29be1ba049ba3
Jerry Chen <onlyuser@gmail.com>
2013-03-24 19:54:43 +0800

    fix make fail

--- variations/0_XLang_full/include/XLang.h
+++ variations/0_XLang_full/include/XLang.h
@@ -90,7 +90,7 @@ int _XLANG_parse(ParserContext*, yyscan_t);
 void _XLANG_error(YYLTYPE* loc, ParserContext* pc, yyscan_t scanner, const char* s);
 void _XLANG_error(const char* s);
 
-std::stringstream &error_messages2();
+std::stringstream &error_messages();
 std::string id_to_name(uint32_t lexer_id);
 
 xl::node::NodeIdentIFace* make_ast(xl::Allocator &alloc, char* s);
