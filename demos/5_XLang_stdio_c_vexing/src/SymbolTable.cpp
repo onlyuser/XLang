d729f688abe6ff135b2f0ce73f925db57e0ffda7
Jerry Chen <onlyuser@gmail.com>
2015-09-15 03:31:03 +0800

    add "most vexing parse" example

---
+++ demos/5_XLang_stdio_c_vexing/src/SymbolTable.cpp
@@ -0,0 +1,108 @@
+#include <SymbolTable.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+Symbol::Symbol(type_t type, std::string name)
+    : m_type(type),
+      m_name(name)
+{
+}
+
+void Symbol::print() const
+{
+    std::stringstream ss;
+    ss << "\"" << m_name << "\"\t: ";
+    switch(m_type) {
+        case TYPE: ss << "type"; break;
+        case FUNC: ss << "func"; break;
+        case VAR:  ss << "var"; break;
+        default:
+            break;
+    }
+    ss << std::endl;
+    std::cout << ss.str();
+}
+
+SymbolTable::SymbolTable()
+{
+}
+
+SymbolTable::~SymbolTable()
+{
+    reset();
+}
+
+SymbolTable* SymbolTable::instance()
+{
+    static SymbolTable g_symbol_table;
+    return &g_symbol_table;
+}
+
+bool SymbolTable::add_type(std::string name)
+{
+    symbols_t::iterator p = m_symbols.find(name);
+    if(p != m_symbols.end()) {
+        std::cout << "Error: Type \"" << name << "\" already exists in symbol table!" << std::endl;
+        return false;
+    }
+    m_symbols.insert(p, symbols_t::value_type(name, new Symbol(Symbol::TYPE, name)));
+    return true;
+}
+
+bool SymbolTable::add_func(std::string name)
+{
+    symbols_t::iterator p = m_symbols.find(name);
+    if(p != m_symbols.end()) {
+        std::cout << "Error: Func \"" << name << "\" already exists in symbol table!" << std::endl;
+        return false;
+    }
+    m_symbols.insert(p, symbols_t::value_type(name, new Symbol(Symbol::FUNC, name)));
+    return true;
+}
+
+bool SymbolTable::add_var(std::string name)
+{
+    symbols_t::iterator p = m_symbols.find(name);
+    if(p != m_symbols.end()) {
+        std::cout << "Error: Var \"" << name << "\" already exists in symbol table!" << std::endl;
+        return false;
+    }
+    m_symbols.insert(p, symbols_t::value_type(name, new Symbol(Symbol::VAR, name)));
+    return true;
+}
+
+bool SymbolTable::lookup(Symbol::type_t *type, std::string name)
+{
+    if(!type) {
+        return false;
+    }
+    for(symbols_t::iterator p = m_symbols.begin(); p != m_symbols.end(); p++) {
+        if((*p).first == name && (*p).second) {
+            *type = (*p).second->type();
+            return true;
+        }
+    }
+    return false;
+}
+
+void SymbolTable::print() const
+{
+    std::cout << "Symbol table:" << std::endl;
+    std::cout << "[name]\t: [meta-type]" << std::endl;
+    for(symbols_t::const_iterator p = m_symbols.begin(); p != m_symbols.end(); p++) {
+        if((*p).second) {
+            (*p).second->print();
+        }
+    }
+}
+
+void SymbolTable::reset()
+{
+    for(symbols_t::iterator p = m_symbols.begin(); p != m_symbols.end(); p++) {
+        if((*p).second) {
+            delete (*p).second;
+        }
+    }
+    m_symbols.clear();
+}
